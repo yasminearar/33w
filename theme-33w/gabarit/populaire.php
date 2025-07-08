41a82fc945e2b0407323afde8fe8dc5118363a51
yasminearar <yasmine27arar@gmail.com>
2025-07-07 21:30:42 -0400

    TP2: Etape2 Fixe duplication des cartes

--- theme-33w/gabarit/populaire.php
+++ theme-33w/gabarit/populaire.php
@@ -5,26 +5,11 @@
           /* affiche l'image « mise en avant » miniature */
           the_post();
 
-          ?>
-            
-            <?php
-            if (in_category('galerie')) {
-              get_template_part('gabarit/galerie');
-            } else { 
-              get_template_part('gabarit/carte');
-              ?>
-              
-            <article class="populaire__carte">
-            <?php the_post_thumbnail('miniature'); ?>
-            <h3><?php the_title(); ?></h3>
-            <?php
-
-              $lien = "<a href=" . get_permalink() .">suite</a>"; 
-              echo "<p>" . wp_trim_words(get_the_excerpt(), 10, $lien) . "</p>";
-            
-            } ?> 
-          </article>
-          <?php
+          if (in_category('galerie')) {
+            get_template_part('gabarit/galerie');
+          } else { 
+            get_template_part('gabarit/carte');
+          }
         }
       }?>
     </div>
